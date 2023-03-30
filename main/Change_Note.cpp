beeabf3b492b96af24f5abdfc5ca67d85e93828c
Alex <sashananava12345@mail.ru>
2023-03-30 16:03:03 +0300

    Доработака функции изменения заметок

--- main/Change_Note.cpp
+++ main/Change_Note.cpp
@@ -7,3 +7,17 @@ void Change(Note& note, std::string nstring)
     note.name = nstring
 }
 
+bool Change(Note& note, std::string number)
+{
+    if(CheckNumber(number))
+    {
+        note.number = number
+        return 1;
+    }
+    else
+    {
+        std::cout <<"Number is not valid";
+        return 0 
+    }
+    
+}
